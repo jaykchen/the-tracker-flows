7c3a9104162f896a245057d458e3d4362d7a8ddd
Ji Chen <jaykchen@gmail.com>
2024-03-18 20:10:19 -0400

    github search funcs separately

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-pub mod issues_tracker;
-use chrono::{Datelike, NaiveDate, Timelike, Utc};
+use chrono::{Datelike, NaiveDate, Timelike, Utc, Duration};
 use dotenv::dotenv;
+use std::env;
 use flowsnet_platform_sdk::logger;
 use github_flows::{get_octo, GithubLogin};
 use octocrab_wasi::{
@@ -13,11 +13,9 @@ use openai_flows::{
     OpenAIFlows,
 };
 use schedule_flows::{schedule_cron_job, schedule_handler};
-use slack_flows::send_message_to_channel;
-
-use chrono::Duration;
-pub use issues_tracker::*;
+use anyhow::anyhow;
 use serde::{Deserialize, Serialize};
+use http_req::{request::{Request, Method}, response::Response, uri::Uri};
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
@@ -30,78 +28,24 @@ pub async fn on_deploy() {
 
 #[schedule_handler]
 async fn handler(body: Vec<u8>) {
-    let _ = search_issue_init().await;
+    let _ = inner(body).await;
 }
 
 pub async fn inner(body: Vec<u8>) -> anyhow::Result<()> {
     let query = "repo:SarthakKeshari/calc_for_everything is:pr is:merged label:hacktoberfest-accepted created:2023-10-01..2023-10-03 review:approved -label:spam -label:invalid";
 
-    // let octocrab = get_octo(&GithubLogin::Default);
+    let issues = search_issues_open(&query).await?;
 
-    // let pulls = octocrab
-    //     .search()
-    //     .issues_and_pull_requests(&query)
-    //     .send()
-    //     .await?;
-    // let pulls = octocrab
-    //     .repos("SarthakKeshari", "calc_for_everything")
-    //     .list_stargazers()
-    //     .send()
-    //     .await?;
-
-    let pulls = search_issues_open(&query).await?;
-
-    for pull in pulls {
-        log::error!("pulls: {:?}", pull);
-        let content = format!("{:?}", pull);
+    for iss in issues {
+        log::error!("issues: {:?}", iss);
+        // let content = format!("{:?}", iss);
         // let _ = upload_to_gist(&content).await?;
         break;
     }
 
-    let _ = send_message_to_channel("ik8", "general", "text".to_string()).await;
-
-    // let pulls = get_per_repo_pull_requests(&query).await?;
-    // for pull in pulls {
-    //     log::info!("pulls: {:?}", pull.url);
-    // }
 
     Ok(())
 }
-/* pub async fn inner(body: Vec<u8>) -> anyhow::Result<()> {
-    dotenv().ok();
-    logger::init();
-
-    // let _ = search_for_initial_hits().await;
-    let pool = PgPool::connect(&env::var("DATABASE_URL")?).await?;
-
-    let issue_id = "https://github.com/jaykchen/issue-labeler/issues/24";
-
-    let res = list_comments(&pool, issue_id).await?;
-    log::info!("Comments: {:?}", res);
-
-    Ok(())
-} */
-
-/* async fn run_db() -> anyhow::Result<()> {
-    dotenv::dotenv().ok();
-    let pool = PgPool::connect(&env::var("DATABASE_URL")?).await?;
-
-    let _ = add_project_test_1(&pool).await?;
-    //
-    // let project_id = "jaykchen/issue-labeler";
-
-    // let res = list_projects(&pool).await?;
-    // println!("Projects: {:?}", res);
-    // let res = list_issues(&pool, project_id).await?;
-    let _ = add_issue_test_1(&pool).await?;
-    let _ = add_comment_test_1(&pool).await?;
-
-    let issue_id = "https://github.com/jaykchen/issue-labeler/issues/24";
-
-    let res = list_comments(&pool, issue_id).await?;
-    println!("Comments: {:?}", res);
-    Ok(())
-} */
 
 pub async fn search_issue_init() -> anyhow::Result<()> {
     let start_date = "2023-10-01";
@@ -136,4 +80,350 @@ pub async fn search_issue_init() -> anyhow::Result<()> {
 
     // let _ = upload_to_gist(&texts).await?;
     Ok(())
+}
+
+pub fn inner_query_by_date_range(
+    start_date: &str,
+    n_days: i64,
+    issue_label: &str,
+    pr_label: &str,
+    is_issue: bool,
+    is_start: bool,
+) -> Vec<String> {
+    // let start_date ="2023-10-01";
+    // let issue_label = "hacktoberfest";
+    // let pr_label = "hacktoberfest-accepted";
+    let start_date =
+        NaiveDate::parse_from_str(start_date, "%Y-%m-%d").expect("Failed to parse date");
+
+    let date_point_vec = (0..20)
+        .map(|i| {
+            (start_date + Duration::days(n_days * i as i64))
+                .format("%Y-%m-%d")
+                .to_string()
+        })
+        .collect::<Vec<_>>();
+
+    let date_range_vec = date_point_vec
+        .windows(2)
+        .map(|x| x.join(".."))
+        .collect::<Vec<_>>();
+
+    let mut out = Vec::new();
+    for date_range in date_range_vec {
+        let query = if is_issue && is_start {
+            format!("label:{issue_label} is:issue is:open no:assignee created:{date_range} -label:spam -label:invalid")
+        } else if is_issue && !is_start {
+            format!("label:{issue_label} is:issue is:closed created:{date_range} -label:spam -label:invalid")
+        } else {
+            format!("label:{pr_label} is:pr is:merged created:{date_range} review:approved -label:spam -label:invalid")
+        };
+        out.push(query);
+    }
+
+    out
+}
+
+pub async fn github_http_post_gql(query: &str) -> anyhow::Result<Vec<u8>> {
+    let token = env::var("GITHUB_TOKEN").expect("github_token is required");
+    let base_url = "https://api.github.com/graphql";
+    let base_url = Uri::try_from(base_url).unwrap();
+    let mut writer = Vec::new();
+
+    let query = serde_json::json!({"query": query});
+    match Request::new(&base_url)
+        .method(Method::POST)
+        .header("User-Agent", "flows-network connector")
+        .header("Content-Type", "application/json")
+        .header("Authorization", &format!("Bearer {}", token))
+        .header("Content-Length", &query.to_string().len())
+        .body(&query.to_string().into_bytes())
+        .send(&mut writer)
+    {
+        Ok(res) => {
+            if !res.status_code().is_success() {
+                log::error!("Github http error {:?}", res.status_code());
+                return Err(anyhow::anyhow!("Github http error {:?}", res.status_code()));
+            }
+            Ok(writer)
+        }
+        Err(_e) => {
+            log::error!("Error getting response from Github: {:?}", _e);
+            Err(anyhow::anyhow!(_e))
+        }
+    }
+}
+
+// pub async fn upload_to_gist(content: &str) -> anyhow::Result<()> {
+//     let octocrab = get_octo(&GithubLogin::Default);
+
+//     let filename = format!("gh_search_{}.txt", Utc::now().format("%d-%m-%Y"));
+
+//     let _ = octocrab
+//         .gists()
+//         .create()
+//         .description("Daily Tracking Report")
+//         .public(false) // set to true if you want the gist to be public
+//         .file(filename, content)
+//         .send()
+//         .await?;
+
+//     Ok(())
+// }
+
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct OuterIssue {
+    pub title: String,
+    pub url: String,
+    pub author: String,
+    pub body: String,
+    pub repository: String,
+    pub repository_stars: i64,
+    pub repository_avatar: String,
+    pub issue_labels: Vec<String>,
+    pub comments: Vec<String>,
+}
+
+pub async fn search_issues_open(query: &str) -> anyhow::Result<Vec<OuterIssue>> {
+    #[derive(Serialize, Deserialize, Clone, Debug)]
+    struct GraphQLResponse {
+        data: Option<Data>,
+    }
+
+    #[derive(Serialize, Deserialize, Clone, Debug)]
+    struct Data {
+        search: Option<Search>,
+    }
+
+    #[derive(Serialize, Deserialize, Clone, Debug)]
+    struct Search {
+        issueCount: Option<i32>,
+        edges: Option<Vec<Edge>>,
+        pageInfo: PageInfo,
+    }
+
+    #[derive(Serialize, Deserialize, Clone, Debug)]
+    struct PageInfo {
+        endCursor: Option<String>,
+        hasNextPage: bool,
+    }
+
+    #[derive(Serialize, Deserialize, Clone, Debug)]
+    struct Edge {
+        node: Option<Issue>,
+    }
+
+    #[derive(Serialize, Deserialize, Clone, Debug)]
+    struct Issue {
+        title: Option<String>,
+        url: Option<String>,
+        body: Option<String>,
+        author: Option<Author>,
+        repository: Option<Repository>,
+        labels: Option<Labels>,
+        comments: Option<Comments>,
+    }
+
+    #[derive(Serialize, Deserialize, Clone, Debug)]
+    struct Author {
+        login: Option<String>,
+    }
+
+    #[derive(Serialize, Deserialize, Clone, Debug)]
+    struct Owner {
+        avatarUrl: Option<String>,
+    }
+
+    #[derive(Serialize, Deserialize, Clone, Debug)]
+    struct Repository {
+        url: Option<String>,
+        stargazers: Option<Stargazers>,
+        owner: Option<Owner>,
+    }
+
+    #[derive(Serialize, Deserialize, Clone, Debug)]
+    struct Stargazers {
+        totalCount: Option<i64>,
+    }
+
+    #[derive(Serialize, Deserialize, Clone, Debug)]
+    struct Labels {
+        edges: Option<Vec<LabelEdge>>,
+    }
+
+    #[derive(Serialize, Deserialize, Clone, Debug)]
+    struct LabelEdge {
+        node: Option<Label>,
+    }
+
+    #[derive(Serialize, Deserialize, Clone, Debug)]
+    struct Label {
+        name: Option<String>,
+    }
+
+    #[derive(Serialize, Deserialize, Clone, Debug)]
+    struct Comments {
+        edges: Option<Vec<CommentEdge>>,
+    }
+
+    #[derive(Serialize, Deserialize, Clone, Debug)]
+    struct CommentEdge {
+        node: Option<Comment>,
+    }
+
+    #[derive(Serialize, Deserialize, Clone, Debug)]
+    struct Comment {
+        author: Option<Author>,
+        body: Option<String>,
+    }
+
+    let first_comments = 10;
+    let first_timeline_items = 10;
+    let mut all_issues = Vec::new();
+    let mut after_cursor: Option<String> = None;
+    let file_path = "issues.txt";
+    let mut file = std::fs::OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(file_path)?;
+    let mut count = 0;
+
+    for _ in 0..10 {
+        let query_str = format!(
+            r#"
+            query {{
+                search(query: "{}", type: ISSUE, first: 100, after: {}) {{
+                    issueCount
+                    edges {{
+                        node {{
+                            ... on Issue {{
+                                title
+                                url
+                                body
+                                author {{
+                                    login
+                                }}
+                                repository {{
+                                    url
+                                    stargazers {{
+                                        totalCount
+                                    }}
+                                    owner {{
+                                        avatarUrl
+                                    }}
+                                }}
+                                labels(first: 10) {{
+                                    edges {{
+                                        node {{
+                                            name
+                                        }}
+                                    }}
+                                }}
+                                comments(first: 10) {{
+                                    edges {{
+                                        node {{
+                                            author {{
+                                                login
+                                            }}
+                                            body
+                                        }}
+                                    }}
+                                }}
+                            }}
+                        }}
+                    }}
+                    pageInfo {{
+                        endCursor
+                        hasNextPage
+                    }}
+                }}
+            }}
+            "#,
+            query.replace("\"", "\\\""),
+            after_cursor
+                .as_ref()
+                .map_or(String::from("null"), |c| format!("\"{}\"", c)),
+        );
+
+        let response_body = github_http_post_gql(&query_str)
+            .await
+            .map_err(|e| anyhow!("Failed to post GraphQL query: {}", e))?;
+
+        let response: GraphQLResponse = serde_json::from_slice(&response_body)
+            .map_err(|e| anyhow!("Failed to deserialize response: {}", e))?;
+
+        if let Some(data) = response.data {
+            if let Some(search) = data.search {
+                for edge in search.edges.unwrap_or_default() {
+                    if let Some(issue) = edge.node {
+                        let labels = issue.labels.map_or(Vec::new(), |labels| {
+                            labels.edges.map_or(Vec::new(), |edges| {
+                                edges
+                                    .iter()
+                                    .filter_map(|edge| {
+                                        edge.node
+                                            .as_ref()
+                                            .map(|label| label.name.clone().unwrap_or_default())
+                                    })
+                                    .collect()
+                            })
+                        });
+                        let temp_str = String::from("");
+                        let comments = issue.comments.map_or(Vec::new(), |comments| {
+                            comments.edges.map_or(Vec::new(), |edges| {
+                                edges
+                                    .iter()
+                                    .filter_map(|edge| {
+                                        edge.node.as_ref().map(|comment| {
+                                            format!(
+                                                "{}: {}",
+                                                comment.author.as_ref().map_or("", |a| a
+                                                    .login
+                                                    .as_ref()
+                                                    .unwrap_or(&temp_str)),
+                                                comment.body.as_ref().unwrap_or(&"".to_string())
+                                            )
+                                        })
+                                    })
+                                    .collect()
+                            })
+                        });
+
+                        all_issues.push(OuterIssue {
+                            title: issue.title.unwrap_or_default(),
+                            url: issue.url.unwrap_or_default(),
+                            author: issue
+                                .author.clone()
+                                .map_or(String::new(), |author| author.login.unwrap_or_default()),
+                            body: issue.body.clone().unwrap_or_default(),
+                            repository: issue
+                                .repository
+                                .clone() // Clone here
+                                .map_or(String::new(), |repo| repo.url.unwrap_or_default()),
+                            repository_stars: issue.repository.clone().map_or(0, |repo| {
+                                repo.stargazers
+                                    .map_or(0, |stars| stars.totalCount.unwrap_or(0))
+                            }),
+                            repository_avatar: issue
+                                .repository
+                                .map_or(String::new(), |repo| {
+                                    repo.owner
+                                        .map_or(String::new(), |owner| owner.avatarUrl.unwrap_or_default())
+                                }),
+                            issue_labels: labels,
+                            comments: comments,
+                        });
+                    }
+                }
+                if search.pageInfo.hasNextPage {
+                    after_cursor = search.pageInfo.endCursor
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    Ok(all_issues)
 }
